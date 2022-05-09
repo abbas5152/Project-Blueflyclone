4bb053a599a73cddbe4d60946bf81ec537186d22
abbas152 <you@example.com>
2022-05-09 15:48:36 +0530

    mypartdone

---
+++ homepage.js
@@ -0,0 +1,75 @@
+document.querySelector('#subs').addEventListener("click",myfunction);
+function myfunction(){
+    alert("You Subscribed Bluefy")
+}
+document.querySelector("#ins").addEventListener("click",function(){
+
+  
+    window.location.href="https://www.instagram.com/bluefly/?hl=en"
+});
+document.querySelector("#fb").addEventListener("click",function(){
+    
+    window.location.href="https://www.facebook.com/Bluefly/"
+});
+
+document.querySelector(".sld").addEventListener("click",function(){
+    
+    window.location.href="login/signup.html"
+});
+document.querySelector(".b1").addEventListener("click",function(){
+    
+    window.location.href="women/womens.html"
+});
+document.querySelector(".b2").addEventListener("click",function(){
+    
+    window.location.href="women/womens.html"
+});
+document.querySelector(".b3").addEventListener("click",function(){
+    
+    window.location.href="women/womens.html"
+});
+document.querySelector("#cat").addEventListener("click",function(){
+    
+    window.location.href="product.html"
+});
+document.querySelector(".sld2").addEventListener("click",function(){
+    
+    window.location.href="login/signin.html"
+});
+document.querySelector("#view").addEventListener("click",function(){
+    
+    window.location.href="mens/mens.html"
+});
+document.querySelector("#new").addEventListener("click",function(){
+    
+    window.location.href="S J/Jewelry/jpage1.html"
+});
+document.querySelector("#trnd").addEventListener("click",function(){
+    
+    window.location.href="S J/under50/jpage2.html"
+});
+document.querySelector("#tbutton").addEventListener("click",function(){
+    
+    window.location.href="sunglasses/sunglasses.html"
+});
+document.querySelector("#t2button").addEventListener("click",function(){
+    
+    window.location.href="mens/mens.html"
+});
+document.querySelector("#t4button").addEventListener("click",function(){
+    
+    window.location.href="S J/Jewelry/jpage1.html"
+});
+
+document.querySelector("#t3button").addEventListener("click",function(){
+    
+    window.location.href="women/womens.html"
+});
+document.querySelector("#box1").addEventListener("click",function(){
+    
+    window.location.href="login/signup.html"
+});
+document.querySelector("#box2").addEventListener("click",function(){
+    
+    window.location.href="login/signup.html"
+});
