aec8c79664ccb7190c7ceda14cb4e8986bc9296e
aditi patil <aditianilpatil1599@gmail.com>
2022-05-09 13:12:29 +0530

    login

---
+++ login/signin.js
@@ -0,0 +1,15 @@
+
+  function validate(){
+    var username = document.getElementById("email").value;
+    var password = document.getElementById("pass").value;
+    if ( username == "" && password == ""){
+    alert ("Login successfully");
+    // window.location = <--"C:\Users\Digambar\Desktop\login\password.html"; --> // Redirecting to main page
+    return false;
+    }
+    
+    }
+
+
+
+
