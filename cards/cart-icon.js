686fd5a3fa28b1dbb71f468ed473806f620bd485
aditi patil <aditianilpatil1599@gmail.com>
2022-05-09 16:38:58 +0530

    add

---
+++ cards/cart-icon.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {myFunction()};
+    
+var header = document.getElementById("tabs");
+
+var stayNav = header.offsetTop;
+function myFunction() {
+    if (window.pageYOffset > stayNav) {
+        header.classList.add("stayNav");
+    }
+    else {
+        header.classList.remove("stayNav");
+    }
+}
